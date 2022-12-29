a234659e30db02d30ea5730497184a93c5361ae2
isabela-reis <isabela_pr@outlook.com>
2022-12-29 12:16:25 -0300

    commit inicial

---
+++ AF.py
@@ -0,0 +1,10 @@
+def soma (Jogo1):
+    for i in range():
+        print(i)
+        i= int(input("Digite o valor que deseja adcionar a lista: "))
+        lista.insere_no_inicio(lista,i)
+        print("Lista: ", lista)
+        if(lista.busca(jogo1)==jogo1):
+        Jogo1 = Jogo1+ i
+    return Jogo1
+        
